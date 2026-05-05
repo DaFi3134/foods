7814d2c86590efd50b103a6c22f6f3fb928c746c
DaFi3134 <143506480+DaFi3134@users.noreply.github.com>
2026-05-05 19:26:34 +0300

    Update submission-config.js

--- docs/js/submission-config.js
+++ docs/js/submission-config.js
@@ -5,7 +5,7 @@
 // Secret/service_role ключи сюда НЕ вставлять.
 window.SUPABASE_CONFIG = {
   url: "https://qygayinuchdngerceupt.supabase.co/rest/v1",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Z2F5aW51Y2hkbmdlcmNldXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5OTYxMTksImV4cCI6MjA5MzU3MjExOX0.B2mV__rdQjDZX0QRpqR4U3TrvvAzkq83enVwrKOEDsQ",
+  anonKey: "sb_publishable_7S_qFgU6NZXyMvOA1RV4Tg_12ZyM4Dq",
   adminEmail: "your-email@example.com"
 };
 
